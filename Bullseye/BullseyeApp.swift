fc5e477803011a659067ef0e2a147f51474b3417
satocom <satocom@gmail.com>
2021-03-01 15:24:44 +0900

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Satoko Omatsu on 2021/03/01.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
